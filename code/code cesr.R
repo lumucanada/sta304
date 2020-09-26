b1182e289e69fafa560ed400110db8bf0e2caf92
lumucanada <lumucanada@gmail.com>
2020-09-26 23:47:25 +0000

    8888888888

--- code/code cesr.R
+++ code/code cesr.R
@@ -6,17 +6,15 @@ library(tidyverse)
 library(devtools)
 # use the data of 2019 election web
 cesR::get_ces(srvy = "ces2019_web")
-class(ces2019_web$cps19_fed_gov_sat )
+
 
 # construct the plot that shows the satisfaction of people for the federal government on a scale 1 to 5
-ces2019_web
-ggplot(data = ces2019_web, aes(x = cps19_fed_gov_sat  )) +
-  geom_bar()
+ces2019_web %>%
+  ggplot( aes(x = cps19_fed_gov_sat  )) + geom_bar()
 
 # construct the plot that shows what party people will vote for
-ces2019_web
-ggplot(data = ces2019_web, aes(x = cps19_votechoice  )) +
-  geom_bar()
+ces2019_web %>%
+  ggplot(aes(x = cps19_votechoice  )) + geom_bar()
 
 #  this plot shows the vote choice of the people who are not satisfied with the government
 ces2019_web1 <-
